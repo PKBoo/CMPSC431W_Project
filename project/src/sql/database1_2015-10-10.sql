40bf3397ad1ea7e82731d20dad145795f2438439
Dylan Nguyen <d_nguyen@live.com>
2015-10-14 13:04:20 -0400

    Remove starting_price attribute from Services table. Add foreign key for Transaction_Items to Transactions

--- project/src/sql/database1_2015-10-10.sql
+++ project/src/sql/database1_2015-10-10.sql
@@ -115,7 +115,6 @@ DROP TABLE IF EXISTS `Services`;
 CREATE TABLE `Services` (
   `service_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) DEFAULT NULL,
-  `starting_price` int(11) DEFAULT NULL,
   `end_date` datetime DEFAULT NULL,
   PRIMARY KEY (`service_id`),
   KEY `fk_item` (`item_id`),
@@ -205,6 +204,8 @@ ALTER TABLE Items ADD CONSTRAINT fk_ItemsUsers FOREIGN KEY (user_id) REFERENCES 
 ALTER TABLE Item_Tags ADD CONSTRAINT fk_ItemTags_Tags FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
   ON DELETE CASCADE;
 ALTER TABLE Categories ADD CONSTRAINT fk_parent_category FOREIGN KEY (parent_id) REFERENCES Categories(category_id)
+  ON DELETE CASCADE;
+ALTER TABLE Transaction_Items ADD CONSTRAINT fk_transaction_items_transaction FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id)
   ON DELETE CASCADE;
 
 
