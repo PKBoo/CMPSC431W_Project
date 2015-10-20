8daaf22033c90587ca0ea4847de2200966732480
Dylan Nguyen <d_nguyen@live.com>
2015-10-19 21:46:03 -0400

    Update User test data with hashed passwords - Default password for all users is 123456

--- project/src/sql/testdata.sql
+++ project/src/sql/testdata.sql
@@ -1,20 +1,19 @@
 insert into Users (username, password, first_name, last_name, email, permissions) values
-('ahoward0','nQwFP9AmZUt','Alan','Howard','ahoward0@noaa.gov',0),
-('kduncan1','rUPvz1','Kathryn','Duncan','kduncan1@cdbaby.com',0),
-('whall2','3ZUXY091mKH','Willie','Hall','whall2@networksolutions.com',0),
-('mbishop3','Vn3lbz9bbFeq','Michelle','Bishop','mbishop3@facebook.com',0),
-('nbutler4','3qIlcg2K9','Nicholas','Butler','nbutler4@vkontakte.ru',0),
-('cadams5','PHFk1TQPh','Craig','Adams','cadams5@geocities.com',0),
-('djackson6','rFinXX9AFWxm','Donald','Jackson','djackson6@usa.gov',0),
-('kowens7','zrELS7HvIy','Kenneth','Owens','kowens7@gov.uk',0),
-('aramirez8','EXKW6aZC7Q5x','Aaron','Ramirez','aramirez8@adobe.com',0),
-('icarpenter9','VJFA0puYz','Irene','Carpenter','icarpenter9@usda.gov',0),
-('cwellsa','Z3PGaq8A','Cynthia','Wells','cwellsa@ed.gov',0),
-('ksimpsonb','4e5FmY','Katherine','Simpson','ksimpsonb@buzzfeed.com',0),
-('tmillsc','5Fgm4C7O','Tammy','Mills','tmillsc@gmpg.org',0),
-('rshawd','j3c0HKP','Ryan','Shaw','rshawd@amazon.com',0),
-('wgriffine','va8RyTKF','Willie','Griffin','wgriffine@issuu.com',0);
-
+('ahoward0','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Alan','Howard','ahoward0@noaa.gov',0),
+('kduncan1','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Kathryn','Duncan','kduncan1@cdbaby.com',0),
+('whall2','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Willie','Hall','whall2@networksolutions.com',0),
+('mbishop3','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Michelle','Bishop','mbishop3@facebook.com',0),
+('nbutler4','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Nicholas','Butler','nbutler4@vkontakte.ru',0),
+('cadams5','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Craig','Adams','cadams5@geocities.com',0),
+('djackson6','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Donald','Jackson','djackson6@usa.gov',0),
+('kowens7','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Kenneth','Owens','kowens7@gov.uk',0),
+('aramirez8','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Aaron','Ramirez','aramirez8@adobe.com',0),
+('icarpenter9','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Irene','Carpenter','icarpenter9@usda.gov',0),
+('cwellsa','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Cynthia','Wells','cwellsa@ed.gov',0),
+('ksimpsonb','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Katherine','Simpson','ksimpsonb@buzzfeed.com',0),
+('tmillsc','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Tammy','Mills','tmillsc@gmpg.org',0),
+('rshawd','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Ryan','Shaw','rshawd@amazon.com',0),
+('wgriffine','$2b$12$I8d7VI1lgrNprvc5/WweE.xVdJBX2/ZGOqEcc0SGEbIvF1aEaBwJG','Willie','Griffin','wgriffine@issuu.com',0);
 
 insert into Items (user_id, category_id, name, price, created_at) values
 (6,18,'Blue Bootstrap Blog Template',4.34,'2015-11-16 7:46:00'),
