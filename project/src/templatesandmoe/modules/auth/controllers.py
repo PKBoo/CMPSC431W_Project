8d262940daa0eeb0ccc4a35b0c8fb4c9c0c7b638
Dylan Nguyen <d_nguyen@live.com>
2015-10-21 09:38:38 -0400

    Add permission to session - Add an admin dashboard link to home page for testing permission level

--- project/src/templatesandmoe/modules/auth/controllers.py
+++ project/src/templatesandmoe/modules/auth/controllers.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, flash, redirect, render_template, session
 from templatesandmoe.modules.auth.forms.login import LoginForm
-from templatesandmoe.modules.users.models import User
 from templatesandmoe.modules.auth.repository import AuthRepository
-from templatesandmoe import db
 
 authModule = Blueprint('auth', __name__)
 authRepository = AuthRepository()
@@ -16,6 +14,7 @@ def login():
         if user:
             session['user_id'] = user.user_id
             session['username'] = user.username
+            session['permission'] = user.permissions
 
             return redirect('/')
         else:
--- project/src/templatesandmoe/modules/main/controllers.py
+++ project/src/templatesandmoe/modules/main/controllers.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, render_template
-from templatesandmoe.modules.users.models import User
-from templatesandmoe import db
 
 mainModule = Blueprint('main', __name__, url_prefix='/')
+
 
 @mainModule.route('/', methods=['GET'])
 def home():
