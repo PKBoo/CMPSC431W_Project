0618b2ec819fb2a20b24382da8a67c6749441be6
Dylan Nguyen <d_nguyen@live.com>
2015-11-25 13:25:12 -0500

    Split expiration field on order page into month/year

--- project/src/templatesandmoe/modules/main/forms/payment_information.py
+++ project/src/templatesandmoe/modules/main/forms/payment_information.py
@@ -1,10 +1,31 @@
 from flask.ext.wtf import Form
-from wtforms.fields import StringField, PasswordField
+from wtforms.fields import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email
+import datetime
 
+def month_choices():
+    choices = []
+    for i in range(1, 13):
+        choices.append((i,i))
+
+    return choices
+
+def year_choices():
+    choices = []
+    now = datetime.datetime.now()
+    current_year = now.year
+
+    for i in range(current_year, current_year + 11):
+        choices.append((i,i))
+    return choices
 
 class PaymentInformationForm(Form):
     name = StringField('Name on Card', validators=[DataRequired(message='Name on card is required.')])
     number = StringField('Card Number', validators=[DataRequired('Card number is required.')])
     expiration = StringField('Expiration', validators=[DataRequired('Expiration date is required.')])
+
+    expiration_month = SelectField('Month', choices=month_choices())
+    expiration_year = SelectField('Year', choices=year_choices())
+
     cvc = StringField('CVC/CW', validators=[DataRequired('Security code is required.')])
+
