e8c205b93b1f7c9fee7864cb48bcb4cf49ef69a9
Dylan Nguyen <d_nguyen@live.com>
2015-12-03 20:55:21 -0500

    Move service routes to items module

--- project/src/templatesandmoe/modules/items/controllers.py
+++ project/src/templatesandmoe/modules/items/controllers.py
@@ -1,15 +1,17 @@
 import os
-from flask import Blueprint, request, render_template, redirect, session
+from flask import Blueprint, flash, request, render_template, redirect, session
 from templatesandmoe.modules.core.pagination import Pagination
 from templatesandmoe import db_session
 from templatesandmoe.modules.items.service import ItemsService
 from templatesandmoe.modules.categories.service import CategoriesService
 from templatesandmoe.modules.ratings.service import RatingsService
+from templatesandmoe.modules.auctions.service import AuctionsService
 
 itemsModule = Blueprint('items', __name__)
 items = ItemsService(database=db_session)
 categories = CategoriesService(database=db_session)
 ratings = RatingsService(database=db_session)
+auctions = AuctionsService(database=db_session)
 
 """
     Show all templates
@@ -81,5 +83,65 @@ def update_rating(item_id):
             ratings.add_rating(item_id, session.get('user_id'), rating)
 
         return redirect(request.referrer)
+    else:
+        return redirect('/login')
+
+
+
+@itemsModule.route('/services/', defaults={'page': 1})
+@itemsModule.route('/services/page/<int:page>')
+def services(page):
+    auctions, count = items.get_filtered_services(page, services_per_page=15)
+    pagination = Pagination(page, 15, count)
+    return render_template('main/services.html',
+                           auctions=auctions,
+                           pagination=pagination)
+
+
+@itemsModule.route('/services/<int:item_id>')
+def single_service(item_id):
+    service = items.get_service_by_id(item_id)
+    bids = auctions.get_bids_for_service(service.service_id)
+    return render_template('main/service.html',
+                           service=service,
+                           bids=bids)
+
+
+@itemsModule.route('/services/<int:item_id>/bids', methods=['POST'])
+def place_bid(item_id):
+    user_id = session.get('user_id')
+    amount = request.form.get('amount')
+    if user_id:
+
+        service = items.get_service_by_id(item_id)
+        highest_bid = auctions.get_highest_bid(service.service_id)
+        if highest_bid is None:
+            bid_check_amount = service.start_price
+        else:
+            bid_check_amount = highest_bid.amount
+
+
+        # Make sure bid amount is a valid currency amount
+        try:
+            amount = float("{0:.2f}".format(float(amount)))
+        except:
+            flash('Bid must be a valid amount.', category='bid')
+            return redirect(request.referrer)
+
+        # Don't allow bidding if current user is the owner of the service
+        # or if the current user is currently the highest bidder (only if bids exist)
+        if service.user_id != user_id and (highest_bid is None or highest_bid.user_id != user_id):
+
+            # if there is a highest bid, make sure the bid amount is greater than it
+            if amount > bid_check_amount:
+                auctions.place_bid(service.service_id, user_id, amount)
+                flash('Successfully placed bid', category='bid_placed')
+                return redirect(request.referrer)
+            else:
+                flash('Bid amount must be greater than highest bid.', category='bid')
+                return redirect(request.referrer)
+        else:
+            flash('You already have the highest bid.', category='bid')
+            return redirect(request.referrer)
     else:
         return redirect('/login')
